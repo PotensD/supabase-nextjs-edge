fb466b7f8a625499e3757032c52ee75b185bb44f
PotensD <me@PotensD.dev>
2023-04-03 18:44:49 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { supabase } from "~/lib/supabaseClient";
 
 export default async function Home() {
-  const { data } = await supabase.from('countries').select('uuid , name , max(id), id');
+  const { data } = await supabase.from('countries').select('id, name, uuid')
+    .is('uuid', null)
 
   return <pre>
     {JSON.stringify(data, null, 2)}
